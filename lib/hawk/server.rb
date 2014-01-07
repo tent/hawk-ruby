36dcf98fff7b39c99f891c0ae422495af9b3522f
Jesse Stuart <jesse@jessestuart.ca>
2014-01-07 11:03:51 -0500

    Fix tests

--- lib/hawk/server.rb
+++ lib/hawk/server.rb
@@ -41,7 +41,8 @@ module Hawk
     end
 
     def build_authorization_header(options)
-      Hawk::AuthorizationHeader.build({ :type => 'response' }.merge(options), [:hash, :ext, :mac])
+      options[:type] = 'response'
+      Hawk::AuthorizationHeader.build(options, [:hash, :ext, :mac])
     end
 
     def build_tsm_header(options)
